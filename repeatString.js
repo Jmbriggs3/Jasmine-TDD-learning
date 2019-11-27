8e2d423c54767baeae1114ef969ba7b8da8d8e9e
Jmbriggs3 <46725493+Jmbriggs3@users.noreply.github.com>
2019-11-27 11:59:06 -0500

    Add files via upload
    using (for) loop to repeat string various # of times

---
+++ repeatString.js
@@ -0,0 +1,14 @@
+let repeatString = function(word,times) {
+    if (times < 0) return 'ERROR' 
+    let string = 'hey' 
+    for (let i = 0; i = 10; i++) {
+        string += word 
+        } 
+        return string; 
+    }
+    
+       
+ 
+ 
+
+module.exports = repeatString
