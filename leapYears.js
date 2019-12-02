8b54c8831eee62a45e771f09bceab1af083f742b
Jmbriggs3 <46725493+Jmbriggs3@users.noreply.github.com>
2019-12-02 18:28:05 -0500

    Add files via upload
    Writing a function in JS that checks to see if a given year is a leap year. The spec file is a test using Jasmine

---
+++ leapYears.js
@@ -0,0 +1,13 @@
+const leapYears = function(year) {
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0); 
+
+} 
+
+leapYears(1996); 
+leapYears(1997); 
+leapYears(34992); 
+leapYears(1900); 
+leapYears(1600);
+leapYears(700);
+
+module.exports = leapYears
