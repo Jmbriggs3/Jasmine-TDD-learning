58329bcbb50d0bfb8cd1e79242d11dfed914e2bc
Jmbriggs3 <46725493+Jmbriggs3@users.noreply.github.com>
2021-07-02 11:47:59 -0400

    Add files via upload
    Add and Subtract JS functions passed on jasmine testing through terminal (initial commit)

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function add (a,b) {
+	return a + b;
+}
+
+function subtract (a,b) {
+	return a-b;
+}
+
+function sum ([]) {
+	return ([...sum]);
+}
+
+function multiply () {
+	
+}
+
+function power() {
+	
+}
+
+function factorial() {
+	
+}
+
+module.exports = {
+	add,
+	subtract,
+	sum,
+	multiply,
+    power,
+	factorial
+}
+	
